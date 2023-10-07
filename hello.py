9d034eb1bf0cdb32f5827fe6c3067d74171d58c8
Alyssa_Rosado <Alyssajrosado@icloud.com>
2023-10-06 20:39:51 -0400

    printed current datetime

---
+++ hello.py
@@ -0,0 +1,12 @@
+import pandas
+from datetime import datetime
+
+# declare my variables 
+my_var = datetime.now()
+
+if __name__ == "__main__":
+    print("program start...")
+
+    print(f'print out : {my_var}') 
+
+    print("program end...")
